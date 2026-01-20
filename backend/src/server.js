5c6b7561ad3f127f9c80d074cd3167962971e002
alexpetkoff <iskander9207@icloud.com>
2026-01-20 10:56:28 +0200

    Update server.js
    remove the usage of path.resolve()

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,14 +10,12 @@ dotenv.config({ path: path.join(__dirname, ".env") }); // point the exact path w
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const _dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontEndPath = path.join(_dirname, "..", "frontend", "dist");
-
+  const frontEndPath = path.join(__dirname, "..", "..", "frontend", "dist");
   app.use(express.static(frontEndPath));
 
   app.get("*", (_, res) => {
